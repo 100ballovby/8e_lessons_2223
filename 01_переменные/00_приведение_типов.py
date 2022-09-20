dfd0c0507b200095420e4350918c3b9c14df20e7
demidraksin <greatraksin@icloud.com>
2022-09-20 11:07:41 +0300

    Adding data types management

---
+++ 01_переменные/00_приведение_типов.py
@@ -0,0 +1,21 @@
+a = 'five'  # str
+b = '2'  # str
+c = '6.2'  # str
+d = 5  # int
+e = 1.75  # float
+
+# print(int(a)) <- нельзя из-за того, что five = 5 только у вас в голове
+print(int(b))  # можно
+# print(int(c))  <- нельзя из-за десятичной точки в строке
+print(int(e))  # можно, но обрежется дробная часть
+
+a = 'five'  # str
+b = '2'  # str
+c = '6.2'  # str
+d = 5  # int
+e = 1.75  # float
+
+print(float(b))
+print(float(c))
+print(float(d))
+
---
+++ 01_переменные/02_bool.py
@@ -0,0 +1,11 @@
+a = 0
+b = 1
+c = 79
+d = -8
+
+print(bool(a), a)  # False
+print(bool(b), b)  # True
+print(bool(c), c)  # True
+print(bool(d), d)  # True
+print(bool(' '), ' ')  # True
+print(bool(None), None)  # False
